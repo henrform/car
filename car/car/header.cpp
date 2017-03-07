6999f47986273aa94837fbe91a5bc33c23157725
henrform <henry.forman@hotmail.com>
2017-03-07 14:49:57 +0100

    Working

--- car/car/header.cpp
+++ car/car/header.cpp
@@ -1,27 +1,38 @@
 #include "header.hpp"
 
 Auto::Auto() {
-	*sector = 2;
+	//Set default Values for Sector 2
+	*sector = 1;
 	*curr_edge = 1;
-	*edge1 = 1;
-	*edge2 = 2;
-	*edge3 = 4;
-	*edge4 = NULL;
+	*edge[0] = 1;
+	*edge[1] = 2;
+	*edge[2] = 4;
+	*edge[3] = NULL;
 }
 void Auto::get_sector() {
+	//Where am I?
 
 }
 
+void Auto::drive() {
+	//drive somewhere
+}
+
+//Chooses the next edge
 void Auto::choose_edge() {
-	if (*edge4 == NULL) {
-		if (*edge3 == NULL) {
-			int randint = rand() % 2;
+	int randint;
+	do {
+		if (*edge[3] == NULL) {
+			if (*edge[2] == NULL)
+				randint = rand() % 2;
+			else
+				randint = rand() % 3;
 		}
-		else {
-			int randint = rand() % 3;
-		}
-	}
-	else {
-		int randint = rand() % 4;
-	}
+		else
+			randint = rand() % 4;
+	} while (map.Sectors[*sector][randint] == *curr_edge);
+	drive_to_edge = &map.Sectors[*sector][randint];
+	drive();
+	curr_edge = drive_to_edge;
 }
+
--- car/car/header.hpp
+++ car/car/header.hpp
@@ -1,29 +1,41 @@
 #pragma once
 #include <iostream>
 
+typedef struct Map {
+	int Sectors[9][4] = { { 1,  2,  NULL, NULL },
+						  { 1,  2,  4,    NULL },
+					      { 2,  5,  NULL, NULL },
+						  { 3,  6,  8,    NULL },
+						  { 4,  6,  7,    9 },
+						  { 5,  7,  10,   NULL },
+						  { 8,  11, NULL, NULL },
+						  { 9,  11, 12,	  NULL },
+						  { 10, 12, NULL, NULL } };
+
+	int Edges[12][2] = { { 1, 2 },
+						 { 2, 3 },
+						 { 1, 4 },
+						 { 2, 5 },
+						 { 3, 6 },
+						 { 4, 5 },
+						 { 5, 6 },
+						 { 4, 7 },
+						 { 5, 8 },
+						 { 7, 8 },
+						 { 8, 9 } };
+} Map;
+
 class Auto {
 private:
-	int *curr_edge = NULL;
-	int *edge1 = NULL;
-	int *edge2 = NULL;
-	int *edge3 = NULL;
-	int *edge4 = NULL;
-	int *sector = NULL;
-
+	int *curr_edge;
+	int *edge[4];
+	int *sector;
+	int *drive_to_edge;
+	Map map;
 public:
 	Auto();
 	void choose_edge();
 	void get_sector();
+	void drive();
 };
 
-typedef struct edge {
-	int sector1;
-	int sector2;
-} edge;
-
-typedef struct sector {
-	int edge1;
-	int edge2;
-	int edge3;
-	int edge4;
-} sector;
